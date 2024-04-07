8b9f185e9b21326233e78a0414df6e95f61cea57
Abel94Phen <phenomenalironfist@gmail.com>
2024-04-08 00:25:20 +0300

    Modified One Solution

--- C_Target_Practice.py
+++ C_Target_Practice.py
@@ -2,9 +2,19 @@ t = int(input())
 for _ in range(t):
     target = []
     for row in range(10):
-        target.append(input().split())
+        target.append(list(input()))
     points = 0
     for i in range(10):
         for j in range(10):
             if target[i][j] == 'X':
-                points += (i + 1)
+                if 4 <= i <= 5 and 4 <= j <= 5:
+                    points += 5
+                elif 3 <= i <= 6 and 3 <= j <= 6:
+                    points += 4
+                elif 2 <= i <= 7 and 2 <= j <= 7:
+                    points += 3
+                elif 1 <= i <= 8 and 1 <= j <= 8:
+                    points += 2
+                else:
+                    points += 1
+    print(points)
