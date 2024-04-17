bbaf7dba73afd6d4caa2dd814581493ae4e9b107
nataldose <nataldose@gmail.com>
2024-04-17 23:02:36 +0300

    Modified One Solution

--- C_Where_s_the_Bishop.py
+++ C_Where_s_the_Bishop.py
@@ -5,10 +5,11 @@ for _ in range(t):
     board = []
     for _ in range(8):
         board.append(input())
-    ans = None
-    for i in range(1,7):
-        for j in range(1,7):
-            if board[i][j]=="#" and board[i+1][j+1]=="#" and board[i+1][j-1]=="#" and board[i-1][j-1]=="#" and board[i-1][j+1]=="#":
-                ans = [i+1,j+1]
+    up, down = 0, 7
+    while board[up].count('#') <= 1:
+        up += 1
+    
+    while board[up].count('#') == 2:
+        up += 1
 
-    print(*ans)
+    print(up + 1, board[up].index('#') + 1)
